f61f54f779c78cf8abcb70c89bf1c3939881e1ce
Benji-Leboe <benjamin.r.leboe@gmail.com>
2018-09-20 02:45:41 +0000

    add helper functions + import to server

--- server.js
+++ server.js
@@ -2,22 +2,27 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
+const cluster       = require('cluster');
+
+const PORT          = process.env.PORT || 8080;
+const ENV           = process.env.ENV || "development";
+const $             = process.env;
+
+const express       = require("express");
+const bodyParser    = require("body-parser");
+const sass          = require("node-sass-middleware");
 const cookieSession = require('cookie-session');
-const memjs       = require('memjs');
-const bcrypt      = require('bcryptjs');
-const app         = express();
+const memjs         = require('memjs');
+const bcrypt        = require('bcryptjs');
+const app           = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
+const knexConfig    = require("./knexfile");
+const knex          = require("knex")(knexConfig[ENV]);
+const morgan        = require('morgan');
+const knexLogger    = require('knex-logger');
+const helper        = require('./helper_functions/helpers');
 
-const $           = process.env;
+
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
@@ -44,97 +49,116 @@ app.use(express.static("public"));
 //configure cookie-session
 app.use(cookieSession({
   name: 'session',
-  keys: [$KEY1, $KEY2]
+  keys: [ $.KEY1, $.KEY2 ]
 }));
 
-/* APP GET ROUTES */
+if (cluster.isMaster) {
 
-// Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+  const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-// Home page - list of subjects in grid format
-app.get("/", (req, res) => {
-  res.render("index");
-});
+  for (let i = 0; i < WORKERS; i++) {
+    cluster.fork();
+  }
 
-// view profile- bio etc.
-app.get("/:user_id/profile", (req, res) => {
-  res.render("user_profile");
-});
+  cluster.on('exit', (worker) => {
 
-// view main user page w/ posts and likes
-app.get("/:user_id", (req, res) => {
-  res.render("user_posts");
-});
+    console.log('Worker %d died:', worker.id);
+    cluster.fork();
 
-// view posts for specific subject
-app.get("/:subject_id", (req, res) => {
-  res.render("subject_list");
-});
+  });
 
-// view post in specific subject 
-//**TODO: Make AJAX function to render over posts
-app.get("/:subject_id/:post_id", (req, res) => {
-  res.render('view_post');
-});
+} else {
 
-/* APP POST ROUTES */
+  /* APP GET ROUTES */
 
-// register user via AJAX and submit json data to DB
-app.post('/register', (req, res) => {
+  // Mount all resource routes
+  app.use("/api/users", usersRoutes(knex));
 
-});
+  // Home page - list of subjects in grid format
+  app.get("/", (req, res) => {
+    res.render("index");
+  });
 
-// check user login and get user data from DB, relay to app.js via AJAX
-app.post('/login', (req, res) => {
+  // view profile- bio etc.
+  app.get("/:user_id/profile", (req, res) => {
+    res.render("user_profile");
+  });
 
-});
+  // view main user page w/ posts and likes
+  app.get("/:user_id", (req, res) => {
+    res.render("user_posts");
+  });
 
-// clear user session
-app.post('/logout', (req, res) = {
+  // view posts for specific subject
+  app.get("/:subject_id", (req, res) => {
+    res.render("subject_list");
+  });
 
-});
+  // view post in specific subject 
+  //**TODO: Make AJAX function to render over posts
+  app.get("/:subject_id/:post_id", (req, res) => {
+    res.render('view_post');
+  });
 
-// submit post, add subject tags, assign unique ID and reference user ID
-// store in DB
-app.post('/post', (req, res) => {
-  
-});
+  /* APP POST ROUTES */
 
-// edit post
-// (user can only edit own post) 
-app.put('/post/:post_id', (req, res) => {
+  // register user via AJAX and submit json data to DB
+  app.post('/register', (req, res) => {
 
-});
+  });
 
-// owner remove post from DB
-app.delete('/post/:post_id', (req, res) => {
+  // check user login and get user data from DB, relay to app.js via AJAX
+  app.post('/login', (req, res) => {
 
-});
+  });
 
-// like post, increment like count, add post to user likes 
-// (user cannot like own post)
-app.put('/like', (req, res) => {
+  // clear user session
+  app.post('/logout', (req, res) = {
 
-});
+  });
 
-// rate post, calculate average and display
-// (user cannot rate own post) 
-app.put('/rate', (req, res) => {
+  // submit post, add subject tags, assign unique ID and reference user ID
+  // store in DB
+  app.post('/post', (req, res) => {
+    
+  });
 
-});
+  // edit post
+  // (user can only edit own post) 
+  app.put('/post/:post_id', (req, res) => {
 
-// submit comment
-app.post('/comment', (req, res) => {
+  });
 
-});
+  // owner remove post from DB
+  app.delete('/post/:post_id', (req, res) => {
 
-// edit comment
-// (user can only edit own comment)
-app.put('comment/:comment_id', (req, res) => {
+  });
 
-});
+  // like post, increment like count, add post to user likes 
+  // (user cannot like own post)
+  app.put('/like', (req, res) => {
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
-});
+  });
+
+  // rate post, calculate average and display
+  // (user cannot rate own post) 
+  app.put('/rate', (req, res) => {
+
+  });
+
+  // submit comment
+  app.post('/comment', (req, res) => {
+
+  });
+
+  // edit comment
+  // (user can only edit own comment)
+  app.put('comment/:comment_id', (req, res) => {
+
+  });
+
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
+
+}
